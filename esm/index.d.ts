3d756c22c38d44417ebae30449eeefd415d4f974
Rory0304 <mitty0304@naver.com>
2025-09-09 22:56:17 +0900

    chore: update .gitignore to exclude /esm directory and remove obsolete ESM files

--- esm/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { PluginConfig } from '@trivago/prettier-plugin-sort-imports';
-import { Options as Options$1 } from 'prettier';
-
-interface Options extends Options$1, PluginConfig {
-}
-declare const options: Options;
-
-export { options as default };
--- esm/index.js
+++
@@ -1,3 +0,0 @@
-var t={printWidth:80,tabWidth:2,arrowParens:"avoid",semi:!0,singleQuote:!0,bracketSameLine:!1,bracketSpacing:!0,trailingComma:"es5",plugins:["@trivago/prettier-plugin-sort-imports"],importOrder:["^react(.*)","<THIRD_PARTY_MODULES>","^app/(.*)$","^pages/(.*)$","^apis/(.*)$","^hooks/(.*)$","^utils/(.*)$","^components/(.*)$","^styles/(.*)$","^types/(.*)$","^[./]"],importOrderSeparation:!0,importOrderSortSpecifiers:!0},r=t;
-
-export { r as default };
